3ee7152bb7c5137f67ddc40c2338eddcaf35dc85
tlavrenyuk <obidnyak@me.com>
2023-07-28 16:14:31 +0200

    fix: web app publisher feed

---
+++ packages/web-app/pages/publisher/[id].tsx
@@ -0,0 +1,33 @@
+import { useRouter } from 'next/router';
+
+import { useGetPublisherNewsFeedLazyQuery } from '../../graphql/schema';
+import { NewsItemModel } from '../../data/data';
+import { NewsCard } from '../../presentation/news/components/NewsCard';
+import { PageLoader } from '../../presentation/common/common.presentation';
+import { useEffect } from 'react';
+
+export default function NewsFeed() {
+  const router = useRouter();
+  const [getPublisherNewsFeed, { data, loading, error }] =
+    useGetPublisherNewsFeedLazyQuery();
+  useEffect(() => {
+    const { id: publisherId } = router.query;
+    if (publisherId) {
+      getPublisherNewsFeed({
+        variables: {
+          id: publisherId as string,
+        },
+      });
+    }
+  }, [router.query]);
+  const news =
+    data?.getPublisher?.news?.items.map(NewsItemModel.fromGraphQL) ?? [];
+  return (
+    <div className="mx-auto w-full max-w-[800px] p-3">
+      {loading && <PageLoader />}
+      {news.map((newsItem) => {
+        return <NewsCard data={newsItem} key={newsItem.id} />;
+      })}
+    </div>
+  );
+}
